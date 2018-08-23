60f031490d13a457aaeded2e1b47827c77b1a7ad
DESKTOP-OQV2L16\Alex <alexstk@hotmail.com>
2018-08-23 05:59:05 -0500

    7. Routing. Beggining 8

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';}
+import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import {Observable, of} from 'rxjs';
@@ -14,5 +14,11 @@ export class HeroService { // provider
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero ${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
